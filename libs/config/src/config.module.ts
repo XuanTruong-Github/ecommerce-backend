cc23f4cc9c737fef7dac8d3ca36dd3c8b971aa34
truong-lx <truonglx@allinbyte.io>
2026-09-13 17:08:41 +0700

    updated

--- libs/config/src/config.module.ts
+++ libs/config/src/config.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from './env.schema';
 
 @Module({
   providers: [ConfigService],
   exports: [ConfigService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+      validate: validateEnv,
+  })]
 })
-export class ConfigModule {}
+export class AppConfigModule {}
